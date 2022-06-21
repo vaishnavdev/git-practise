49f6c7f92ff30829597605c842bdd64ce96a7b4d
vaishnavdev <vaishnavkannan05@gmail.com>
2022-06-21 15:25:39 +0530

    added properties to caloriesCalculationFile

--- JavaProject/NutritionApp/CaloriesCalculationService.java
+++ JavaProject/NutritionApp/CaloriesCalculationService.java
@@ -1,5 +1,9 @@
 package JavaProject.NutritionApp;
 
 public class CaloriesCalculationService {
+
+    private String calculatedCalorieValue;
+    private String quantity;
+    private int defaultCalorie;
     
 }
